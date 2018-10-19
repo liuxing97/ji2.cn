d4ea0c722c975e3ad0063e946b5d9c8d64e65014
liuxing <gex1217@censng.com>
2018-10-19 15:16:18 +0800

    ''

--- app/Http/Middleware/ClientVisit.php
+++ app/Http/Middleware/ClientVisit.php
@@ -128,20 +128,23 @@ class ClientVisit
 
     public function getIp()
     {
-        $ip=false;
-        if(!empty($_SERVER["HTTP_CLIENT_IP"])){
-            $ip = $_SERVER["HTTP_CLIENT_IP"];
-        }
-        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ips = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
-            if ($ip) { array_unshift($ips, $ip); $ip = FALSE; }
-            for ($i = 0; $i < count($ips); $i++) {
-                if (!eregi ("^(10│172.16│192.168).", $ips[$i])) {
-                    $ip = $ips[$i];
-                    break;
-                }
+            static $realip = NULL;
+
+            if ($realip !== NULL) {
+                return $realip;
             }
-        }
-        return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
+
+            if (getenv( 'HTTP_X_FORWARDED_FOR')) {
+                $realip = getenv( 'HTTP_X_FORWARDED_FOR');
+            } elseif (getenv( 'HTTP_CLIENT_IP')) {
+                $realip = getenv( 'HTTP_CLIENT_IP');
+            } else {
+                $realip = getenv( 'REMOTE_ADDR');
+            }
+
+            preg_match("/[\d\.]{7,15}/", $realip, $onlineip);
+            $realip = !empty($onlineip[0]) ? $onlineip[0] : '0.0.0.0';
+
+            return $realip;
     }
 }
